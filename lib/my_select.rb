e66fdab24017d07c95e0013c34377accebecd9b3
Ryan Wolfe <ryan@studiohopfitness.com>
2019-01-29 23:11:11 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,11 @@ def my_select(collection)
   i = 0
   ary = []
   while i < collection.length
-    ary << yield(collection[i])
+  if  ary << yield(collection[i]) == false
+    nil
+  else
+    collection
+  end
        i += 1
   end
   ary
